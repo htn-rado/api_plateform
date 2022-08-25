3b40782e965d009baf23940af50e4533d3dc4f7c
htn-rado <radohtn16@gmail.com>
2022-08-25 16:07:07 +0300

    partie 2, 3, 4 et 5

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,13 +2,40 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
 
 /**
  * @ORM\Entity(repositoryClass=PostRepository::class)
- * @ApiResource()
+ *
+ * @ApiResource(
+ *     paginationItemsPerPage=2,
+ *     paginationMaximumItemsPerPage=2,
+ *     paginationClientItemsPerPage=true,
+ *     attributes={
+ *          "normalization_context"={"groups"={"read:collection"}},
+ *          "denormalization_context"={"groups"={"write:Post"}}
+ *     },
+ *     collectionOperations={
+ *          "get",
+ *          "post"={"validation_groups": {"create:Post"}}
+ *     },
+ *     itemOperations={
+ *          "put",
+ *          "delete",
+ *          "patch",
+ *          "get"={"normalization_context"={"groups"={"read:item", "read:collection", "read:Post"}}}
+ *     }
+ * )
+ *
+ * @ApiFilter(SearchFilter::class, properties={"id": "exact", "title": "partial"})
+ * @ApiFilter(DateFilter::class, properties={"createdAt"})
  */
 class Post
 {
@@ -16,36 +43,44 @@ class Post
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"read:collection"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read:collection", "write:Post"})
+     * @Assert\Length(min=5, groups={"create:Post"})
      */
     private $title;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read:collection", "write:Post"})
      */
     private $slug;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"read:item", "write:Post"})
      */
     private $content;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"read:item"})
      */
     private $createdAt;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"read:item"})
      */
     private $updatedAt;
 
     /**
      * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="posts")
+     * @Groups({"read:item", "write:Post"})
      */
     private $category;
 
@@ -64,6 +99,12 @@ class Post
         $this->title = $title;
 
         return $this;
+    }
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
     }
 
     public function getSlug(): ?string
